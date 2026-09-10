66500b2531c6ebb51ef49c0684d34c66b33eee57
patilsankett <patilsanket7709@gmail.com>
2026-09-10 13:58:21 +0530

    version 1.0.0

---
+++ demo.py
@@ -0,0 +1,46 @@
+""" 
+int (input("Enter Your number"))
+print(type(a))
+b =10
+c=a+b
+print("this is a+b",c) 
+print(f"this is {a}")
+
+20>10 
+"""
+# Program to check if number is odd or even return true if odd
+"""
+a=int (input("number"))
+print(f"{a} is odd is ",a%2==1)
+print(f"{a} is odd is ",a%2!=0)
+"""
+#Program to print age in days
+"""
+age=int(input("Age:"))
+b=age*365
+print(f"{age} years = {b} days")
+print(age,"years=",b,"days")
+"""
+#convert minutes into hours 
+"""
+minutes=int(input("enter"))
+b=minutes%60
+print(minutes,"=",minutes//60,"hour",b,"minutes")
+"""
+
+#program to extract last digit of a number 
+"""
+a=int(input("Enter the Number"))
+b=a%10
+print(a,": last digit is",b)
+"""
+
+#program to check if person is student or teacher with age below 21
+
+"""
+age=int(input("Enter Your Age : "))
+role=input("Are You Teacher or Student : ")
+print("Eligible :",age<=21)
+"""
+
+#Program to swap two variable without a third variable using arithmetic operations 
